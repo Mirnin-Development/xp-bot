42c6979fd36c980ffb3e4f95bb0bb1e36417d993
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:39:01 -0800

    Create helpadmin.js

---
+++ commands/help/helpadmin.js
@@ -0,0 +1,9 @@
+module.exports ={
+name: "helpadmin",
+code: `
+$title[1;Admin Commands]
+$addField[1;Commands; ban, giverole, kick, memory, slowmode]
+$footer[1;Made by ZAPP DEVELOPMENT]
+$color[1;GREEN]
+`
+}
