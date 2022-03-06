9ef6fb8b99bcb6a5d120fa04988da75e67ae027b
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:38:02 -0800

    Create help.js

---
+++ commands/help/help.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "help",
+    code: `
+    $title[1;Help Menu - PREFIX x!]
+	$addField[1;Sub-Menus; helpadmin, helputil, hepfun, helpeco, helpother]
+    $addField[1;Bot Still In Development;Join The Support Server For Updates]
+    $footer[1;Made by ZAPP DEVELOPMENT with 💖]
+    $addbutton[1;Support Server;5;https://dsc.gg/zapp-dev;enabled;]
+    $addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
+    $color[1;BLUE]
+    $reply
+    `
+}
