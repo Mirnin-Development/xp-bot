59b38ce8f3ca48973c96f83e819dafd04f322881
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:40:47 -0800

    Create helputil.js

---
+++ commands/help/helputil.js
@@ -0,0 +1,9 @@
+module.exports ={
+name: "helputil",
+code: `
+$title[1;Utility Commands]
+$addField[1;Commands; giveaway, membercount, purge, uptime]
+$footer[1;Made by ZAPP DEVELOPMENT]
+$color[1;GREEN]
+`
+}
