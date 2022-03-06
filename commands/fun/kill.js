1a7c5deb923f2f05e0ae63d5f81b67a7a50f92c0
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 13:51:48 -0800

    Create kill.js

---
+++ commands/fun/kill.js
@@ -0,0 +1,9 @@
+module.exports = ({
+name: "kill",
+usage: "kill <member>",
+code: `
+$description[1;$randomText[You were caught and killed by an officer but also succeeded to kill <@$mentioned[1]>;<@$mentioned[1]> killed themselves before you could kill them;You we're caught killing <@$mentioned[1]> and put on death row, sad]]
+$image[1;https://d.img.vision/boltsdrive/download_(1).jpeg]
+$color[1;RED]
+$addTimestamp[1]`
+});
