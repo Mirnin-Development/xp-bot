13e4db33aa873a51aa8b92868e7cc8536bc9c39d
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:51:00 -0800

    Create ping.js

---
+++ commands/other/ping.js
@@ -0,0 +1,8 @@
+module.exports = ({
+    name: "ping",
+    code: `    $title[1;Ping]
+    $addField[1;Current Bot Ping; $ping / ms]
+    $footer[1;Made by ZAPP DEVELOPMENT ™]
+    $color[1;BLUE]
+    $reply`
+})
