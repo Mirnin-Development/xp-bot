2b142d4f148584bf856e8a05caddd845afef84d9
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:39:28 -0800

    Create helpeco.js

---
+++ commands/help/helpeco.js
@@ -0,0 +1,12 @@
+module.exports ={
+name: "helpeco",
+code: `
+$title[1;Economy Commands]
+$addField[1;Commands; COMING SOON!]
+$addField[1;Support; Join our support server for assistance!]
+$footer[1;Made by ZAPP DEVELOPMENT]
+$addbutton[1;Support Server;5;https://dsc.gg/zapp-dev;enabled;]
+$addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
+$color[1;GREEN]
+`
+}
