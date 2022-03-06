431ca77f3977ba70b22848b3d9c3d1140cb545e9
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:40:25 -0800

    Create helpother.js

---
+++ commands/help/helpother.js
@@ -0,0 +1,9 @@
+module.exports ={
+name: "helpoher",
+code: `
+$title[1;Other Commands]
+$addField[1;Commands; avatar, botinfo, botstats, ping]
+$footer[1;Made by ZAPP DEVELOPMENT]
+$color[1;GREEN]
+`
+}
