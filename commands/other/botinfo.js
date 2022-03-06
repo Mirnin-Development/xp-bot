241040f26076b34e7f5f22b750331c5861d98965
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:49:53 -0800

    Create botinfo.js

---
+++ commands/other/botinfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "botinfo",
+    code: `
+    $title[1;Infomation About ($username[$clientID])]
+    $addField[1;I'm XP Bot, I'm Coded in Aoi.js.]
+    $addField[1;I'm a super powerful bot with over a hundred\n commands to come in the future!]
+	$addField[1;Whos My Dev?;My Developer Is DaBoltGod]
+    $footer[1;Made by ZAPP DEVELOPMENT ™]
+    $addbutton[1;Support Server;5;https://dsc.gg/zapp-dev;enabled;]
+    $addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
+    $color[1;BLUE]
+    `
+};
