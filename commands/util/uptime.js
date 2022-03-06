37f85f096ff6a1f925ea89e4a1973edd635d6fb5
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-03-06 09:53:46 -0800

    Create uptime.js

---
+++ commands/util/uptime.js
@@ -0,0 +1,10 @@
+module.exports = ({
+    name: "uptime",
+    code: `    
+    $title[1;Bot Uptime]
+    $addField[1;Uptime;$uptime]
+    $footer[1;Powered by ZAPP DEVELOPMENT ™]
+    $addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
+    $color[1;GREEN]
+    $reply`
+})
