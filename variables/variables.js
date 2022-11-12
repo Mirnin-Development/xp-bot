0c5368eb45b5b844cb5d4c8e6f52d31833bdada3
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:38:21 -0800

    Create variables.js

---
+++ variables/variables.js
@@ -0,0 +1,16 @@
+module.exports = {
+  prefix: "x!",
+  database: "./database/main/main_scheme_1.sql",
+  color: "BLUE",
+ welcomemsg: "undefined",
+ welcomechannel: "undefined",
+ WelcomeChannel: "",
+ WelcomeMessage: "Welcome {user.ping} To {server}!",
+ WelcomeTitle:"Welcome!",
+ ownerid: "12345678987654321",
+ ratelimitlogs: ""
+}
+
+//Made by DaBoltGod
+// https://daboltgod.ml
+// https://mirnindev.cf
