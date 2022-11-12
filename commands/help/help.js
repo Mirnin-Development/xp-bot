9f97cf909c6f0d2cfa118753ed2276f7c11e33fa
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:43:04 -0800

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -4,9 +4,9 @@ module.exports = {
     $title[1;Help Menu - PREFIX x!]
 	$addField[1;Sub-Menus; helpadmin, helputil, hepfun, helpeco, helpother]
     $addField[1;Bot Still In Development;Join The Support Server For Updates]
-    $footer[1;Made by ZAPP DEVELOPMENT with 💖]
-    $addbutton[1;Support Server;5;https://dsc.gg/zapp-dev;enabled;]
-    $addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
+    $footer[1;Made by Mirnin DEVELOPMENT with 💖]
+    $addbutton[1;Support Server;5;https://dc.mirnindev.cf;enabled;]
+    $addbutton[2;Bot Invite;5;https://mirnindev.cf/xpbot;enabled;]
     $color[1;BLUE]
     $reply
     `
