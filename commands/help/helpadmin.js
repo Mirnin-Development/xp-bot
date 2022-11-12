cfa546c5e1c42755b1b6ac4262b5aca55080d2e1
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:44:07 -0800

    Update helpadmin.js

--- commands/help/helpadmin.js
+++ commands/help/helpadmin.js
@@ -3,7 +3,9 @@ name: "helpadmin",
 code: `
 $title[1;Admin Commands]
 $addField[1;Commands; ban, giverole, kick, memory, slowmode]
-$footer[1;Made by ZAPP DEVELOPMENT]
-$color[1;GREEN]
+$footer[1;Made by MIRNIN DEVELOPMENT with 💖]
+$addbutton[1;Support Server;5;https://dc.mirnindev.cf;enabled;]
+$addbutton[2;Bot Invite;5;https://mirnindev.cf/xpbot;enabled;]
+$color[1;$getVar[color]]
 `
 }
