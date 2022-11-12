1a2003f8209dae871b9e62bd118835ac3aae1ea7
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:45:18 -0800

    Update helputil.js

--- commands/help/helputil.js
+++ commands/help/helputil.js
@@ -3,7 +3,9 @@ name: "helputil",
 code: `
 $title[1;Utility Commands]
 $addField[1;Commands; giveaway, membercount, purge, uptime]
-$footer[1;Made by ZAPP DEVELOPMENT]
-$color[1;GREEN]
+$footer[1;Made by MIRNIN DEVELOPMENT with 💖]
+$addbutton[1;Support Server;5;https://dc.mirnindev.cf;enabled;]
+$addbutton[2;Bot Invite;5;https://mirnindev.cf/xpbot;enabled;]
+$color[1;$getVar[color]]
 `
 }
