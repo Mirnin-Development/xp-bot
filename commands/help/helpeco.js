3a616bc0b4e18b45525748d6875f66ec321a7dbc
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:44:24 -0800

    Update helpeco.js

--- commands/help/helpeco.js
+++ commands/help/helpeco.js
@@ -4,9 +4,9 @@ code: `
 $title[1;Economy Commands]
 $addField[1;Commands; COMING SOON!]
 $addField[1;Support; Join our support server for assistance!]
-$footer[1;Made by ZAPP DEVELOPMENT]
-$addbutton[1;Support Server;5;https://dsc.gg/zapp-dev;enabled;]
-$addbutton[2;Bot Invite;5;https://dsc.gg/xp-bot;enabled;]
-$color[1;GREEN]
+$footer[1;Made by MIRNIN DEVELOPMENT with 💖]
+$addbutton[1;Support Server;5;https://dc.mirnindev.cf;enabled;]
+$addbutton[2;Bot Invite;5;https://mirnindev.cf/xpbot;enabled;]
+$color[1;$getVar[color]]
 `
 }
