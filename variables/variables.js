3c2d3d914ef28e2095ed380b06b677591e2f259b
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:41:08 -0800

    Update variables.js

--- variables/variables.js
+++ variables/variables.js
@@ -1,14 +1,28 @@
 module.exports = {
+  
+  //Prefix (You can change this per server!)
   prefix: "x!",
+  
+  //Database #2
   database: "./database/main/main_scheme_1.sql",
-  color: "BLUE",
+  
+  //Embed Color
+  color: "GREEN",
+  
+  //Welcome System
  welcomemsg: "undefined",
  welcomechannel: "undefined",
  WelcomeChannel: "",
  WelcomeMessage: "Welcome {user.ping} To {server}!",
  WelcomeTitle:"Welcome!",
- ownerid: "12345678987654321",
- ratelimitlogs: ""
+  
+  //Owner ID for Owner Commands
+ ownerid: "758791280980918292",
+  
+  //Economy (Comming Soon!)
+    XP: "50",
+    Bank: "500",
+    Wallet: "500"
 }
 
 //Made by DaBoltGod
