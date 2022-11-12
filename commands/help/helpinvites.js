0b34ec0bd799bd88f1686c885ca7b8fe4e9816b2
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2022-11-12 14:44:41 -0800

    Update helpinvites.js

--- commands/help/helpinvites.js
+++ commands/help/helpinvites.js
@@ -3,7 +3,9 @@ name: "helpinvites",
 code: `
 $title[1;Invites Commands]
 $addField[1;Commands; invites, reset]
-$footer[1;Made by ZAPP DEVELOPMENT]
-$color[1;GREEN]
+$footer[1;Made by MIRNIN DEVELOPMENT with 💖]
+$addbutton[1;Support Server;5;https://dc.mirnindev.cf;enabled;]
+$addbutton[2;Bot Invite;5;https://mirnindev.cf/xpbot;enabled;]
+$color[1;$getVar[color]]
 `
 }
